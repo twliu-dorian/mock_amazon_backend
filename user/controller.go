ce1c1e96062468270c94c5a6ecafcaf15a088cc0
twliu-dorian <dorianliu4231@gmail.com>
2023-12-26 14:06:17 +0800

    feat: user basic types
    Signed-off-by: twliu-dorian <dorianliu4231@gmail.com>

--- user/controller.go
+++ user/controller.go
@@ -1,1 +1,24 @@
 package user
+
+type Controller struct{}
+
+func (Controller) Create(c echo.Context) (err error) {
+	var request CreateUserRequest
+	if err = c.Bind(&request); err != nil {
+		err = new(apierror.ApiError).From(err).SetCode(apierror.INPUT_ERROR)
+		return
+	}
+	if err = request.Validate(); err != nil {
+		err = new(apierror.ApiError).From(err).SetCode(apierror.INPUT_ERROR)
+		return
+	}
+
+	toCreate := request.ToWallet()
+	err = CreateUser(toCreate)
+	if err != nil {
+		err = new(apierror.ApiError).From(err)
+		return
+	}
+	
+	return util.AcceptedResponse(c, taskId)
+}
--- user/type.go
+++ user/type.go
@@ -1,1 +1,9 @@
 package user
+
+type Request struct {
+	UserId string    `json:"adminId"`
+	Email   string    `json:"email"`
+	Salt         util.Bytes    `json:"-" db:"salt"`
+	PasswordHash util.Bytes    `json:"-" db:"password_hash"`
+	Role    AdminRole `json:"role"`
+}
